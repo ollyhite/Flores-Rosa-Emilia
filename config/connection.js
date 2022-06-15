6e55d37c83e6f6b2719d8071b392d37f3742e21b
Tbathurst715 <timbathurst@yahoo.com>
2022-06-15 11:33:36 -0600

    Created cbasic connection to DB in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,15 @@
+require('dotenv').config();
 
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.FLOWERSDB_URL
+  ? new Sequelize(process.env.FLOWERSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
