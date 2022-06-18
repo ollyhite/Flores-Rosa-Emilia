7618e44180c13cf3107210e2c130e7c7395ca198
SabotMBT <104104069+SabotMBT@users.noreply.github.com>
2022-06-18 16:05:08 -0600

    console logging values

--- public/js/forCart.js
+++ public/js/forCart.js
@@ -1,11 +1,17 @@
 const addcart = document.getElementById("addcartbtn");
 const numberSel = document.getElementById("quantity");
-const quantity = numberSel.value;
+// const quantity = numberSel.value;
 const product_id = parseInt(addcart.dataset.a);
 const price = parseInt(addcart.dataset.b);
 const user_id = addcart.dataset.c;
+let quantity = "";
+function checkTextbox(element) {
+  var check = element.value;
+  quantity = check;
+  return quantity;
+}
 
-console.log(numberSel);
+console.log(quantity);
 
 function test1() {
   console.log(user_id);
