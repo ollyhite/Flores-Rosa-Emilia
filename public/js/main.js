4a0b0a79105eb06b744e7361c6d3a12a90f2bc09
SabotMBT <104104069+SabotMBT@users.noreply.github.com>
2022-06-20 14:59:56 -0600

    added translation data

--- public/js/main.js
+++ public/js/main.js
@@ -1,46 +1,48 @@
-const changeLang = (languageCode) => {
-        event.preventDefault();
-       console.log(languageCode);
-       document.documentElement.setAttribute("lang", languageCode);
-      };
+// const changeLang = (languageCode) => {
+//         event.preventDefault();
+//        console.log(languageCode);
+//        document.documentElement.setAttribute("lang", languageCode);
+//       };
 
-//top btn 
+//top btn
 mybutton = document.getElementById("topBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "flex";
-    } else {
-        mybutton.style.display = "none";
-    }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "flex";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
 //cart num
 
-const carNum = localStorage.getItem('numbersOfList');
+const carNum = localStorage.getItem("numbersOfList");
 // console.log(carNum);
-const carNumEl = document.getElementById('lblCartCount');
-const addBtn = document.getElementsByClassName('addcart-btn')
+const carNumEl = document.getElementById("lblCartCount");
+const addBtn = document.getElementsByClassName("addcart-btn");
 // const val = parseInt(carNumEl.innerHTML);
 // const quantity = document.querySelector('.number-select').value;
 
 if (!carNum || carNum === 0 || carNum === "0") {
-    localStorage.setItem('numbersOfList', 0)
-    carNumEl.setAttribute('css', 'display:none;')
+  localStorage.setItem("numbersOfList", 0);
+  carNumEl.setAttribute("css", "display:none;");
 } else {
-    // const newNum = val + parseInt(quantity);
-    console.log(carNum);
-    while (carNumEl.firstChild) {
-        carNumEl.removeChild(carNumEl.firstChild);
-    }
-    carNumEl.appendChild(document.createTextNode(carNum));
+  // const newNum = val + parseInt(quantity);
+  console.log(carNum);
+  while (carNumEl.firstChild) {
+    carNumEl.removeChild(carNumEl.firstChild);
+  }
+  carNumEl.appendChild(document.createTextNode(carNum));
 }
